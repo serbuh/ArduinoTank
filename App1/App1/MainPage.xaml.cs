268580d501a1a8923844ebdc935b27dd574ec7ae
Yulia <yukuzntcva@gmail.com>
2017-05-12 20:04:34 +0300

    Added an app solution

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Devices.Enumeration;
+using Windows.Networking.Sockets;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RfcommDeviceService _service;
+        private StreamSocket _socket;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                var devices =
+                      await DeviceInformation.FindAllAsync(
+                        RfcommDeviceService.GetDeviceSelector(
+                          RfcommServiceId.SerialPort));
+
+                var device = devices.Single(x => x.Name == "HC-05");
+
+                _service = await RfcommDeviceService.FromIdAsync(
+                                                        device.Id);
+
+                _socket = new StreamSocket();
+
+                await _socket.ConnectAsync(
+                      _service.ConnectionHostName,
+                      _service.ConnectionServiceName,
+                      SocketProtectionLevel.
+                      BluetoothEncryptionAllowNullAuthentication);
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+            }
+        }
+
+        private async void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                await _socket.CancelIOAsync();
+                _socket.Dispose();
+                _socket = null;
+                _service.Dispose();
+                _service = null;
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+            }
+        }
+
+        private async void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            int dummy;
+
+            if (!int.TryParse(tbInput.Text, out dummy))
+            {
+                tbError.Text = "Invalid input";
+            }
+
+            var noOfCharsSent = await Send(tbInput.Text);
+
+            if (noOfCharsSent != 0)
+            {
+                tbError.Text = noOfCharsSent.ToString();
+            }
+
+        }
+
+        private async Task<uint> Send(string msg)
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                var writer = new DataWriter(_socket.OutputStream);
+
+                writer.WriteString(msg);
+
+                // Launch an async task to 
+                //complete the write operation
+                var store = writer.StoreAsync().AsTask();
+
+                return await store;
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+
+                return 0;
+            }
+        }
+
+        private void tbInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            return;
+        }
+    }
+}
+
