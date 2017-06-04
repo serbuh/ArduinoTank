0d5c628a66f0f132260da19ffb10889a0ac59be2
Yulia <yukuzntcva@gmail.com>
2017-06-04 19:54:35 +0300

    Uploaded new version of app

---
+++ WelcomePage/WelcomePage/UserId.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WelcomePage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserId : Page
+    {
+        public UserId()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var dataSender = new Sender();
+            //dataSender.Initialize();
+            this.Frame.Navigate(typeof(Joystick), null);
+        }
+
+        private void StatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Statistics), null);
+        }
+    }
+}
